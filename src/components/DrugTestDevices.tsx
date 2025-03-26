5e6cccc1180f85f61a1d75f9b0ef432fb0eadf0d
creaserviceProf <creaservice.prof@gmail.com>
2025-03-26 22:21:25 +0100

    Update DrugTestDevices.tsx

--- src/components/DrugTestDevices.tsx
+++ src/components/DrugTestDevices.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DrugTestDevices = () => {
   return (
-    <div className="mt-4 mb-8 relative animate-fade-in overflow-hidden -mx-6">
+    <div className="mt-4 mb-8 relative animate-fade-in overflow-hidden w-full">
       <img 
         src="/lovable-uploads/06dc8068-a4c7-456e-b50d-a3339bc0091f.png" 
         alt="Drug Testing Devices" 
